e1cf6e3eb3e862bce14036e7475e3963f44d9c88
Ronny Tomasetti <ronny.tomasetti@gmail.com>
2016-09-21 19:26:16 -0400

    Add make reservation html file.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/reservations', function(req, res, next) {
-  res.end("GET RESERVATIONS ROUTE")
+  res.end("GET RESERVATIONS ROUTE");
 });
 
 router.post('/reservations', function(req, res, next) {
-	
+
 });
 
 module.exports = router;
