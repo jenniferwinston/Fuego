aeb61da8f2e99ac71bf9cef5424dda1c1578a7eb
Jennifer Winston <winston.jennifer9@gmail.com>
2016-09-21 20:21:05 -0400

    background stlying

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,12 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	console.log("HITTING HOMEPAGE");
 	res.sendFile('index.html');
 });
 
-router.get('/tables', function(req, res, next) {
+router.get('/view-tables', function(req, res, next) {
+	res.sendFile('view-tables.html');
+});
 
+router.get('/make-reservation', function(req, res, next) {
+	res.sendFile('make-reservation.html');
 });
 
 module.exports = router;
