fb324e7e2a38b92b02e05b5ec04962221782dd96
Ronny Tomasetti <ronny.tomasetti@gmail.com>
2016-09-21 20:57:24 -0400

    Fix router bug.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,11 +1,11 @@
 var path = require('path');
 
 module.exports = function(app) {
-	router.get('/reservations', function(req, res, next) {
+	app.get('/reservations', function(req, res, next) {
 		res.end("GET RESERVATIONS ROUTE");
 	});
 
-	router.post('/reservations', function(req, res, next) {
+	app.post('/reservations', function(req, res, next) {
 		console.log("RESERVATIONS POST ROUTE");
 	});
 };
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,15 +1,15 @@
 var path = require('path');
 
 module.exports = function(app) {
-	router.get('/', function(req, res, next) {
+	app.get('/', function(req, res, next) {
 		res.sendFile(path.join(__dirname, 'index.html'));
 	});
 
-	router.get('/view-tables', function(req, res, next) {
+	app.get('/view-tables', function(req, res, next) {
 		res.sendFile(path.join(__dirname, 'view-tables.html'));
 	});
 
-	router.get('/make-reservation', function(req, res, next) {
+	app.get('/make-reservation', function(req, res, next) {
 		res.sendFile(path.join(__dirname, 'make-reservation.html'));
 	});
 };
