4e15e4c8c39e014efaf1d8ecaac4b908a1e42bcb
Ronny Tomasetti <ronny.tomasetti@gmail.com>
2016-09-21 20:58:50 -0400

    Fixed app bug.

--- server.js
+++ server.js
@@ -3,13 +3,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-require('./routes/api-routes.js')(app);
-require('./routes/html-routes.js')(app);
-
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = 3000;
+
+require('./routes/api-routes.js')(app);
+require('./routes/html-routes.js')(app);
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
